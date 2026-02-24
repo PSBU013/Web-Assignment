dd79fd82ecfdf0945ce6059e3ce64774311b7984
Partha Sarathi Biswas <partha.cse10.bu12@gmail.com>
2026-02-25 00:32:56 +0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,12 @@
 <?php
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "blogsystem";
+    $hostname = "sql308.ezyro.com";
+    $username = "EZYRO_41238432";
+    $password = "Me01741ooo898";
+    $dbname = "ezyro_41238432_Blogsystem";
 
     $conn = new mysqli($hostname,$username,$password,$dbname);
 
     if($conn->connect_error){
         die("Database Connection failed");
-    } else {
-        echo("Database Connected");
     }
 ?>
